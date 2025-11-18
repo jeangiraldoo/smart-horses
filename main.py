34a5d13b2873cfed164c1f2efe041371ae05660e
Jean Giraldo <jeann.giraldo@gmail.com>
2025-11-18 15:09:10 -0500

    refactor(GUI): add `Modes` to make state transitions less error prone

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from settings import *
 from colours import COLOURS
 from button import Button
 from game import Game
+from enum import Enum, auto
 
 # pygame.init()
 TITLE_FONT = pygame.font.Font(None, 70)
@@ -47,9 +48,14 @@ CELLS = {
 
 
 class GUI:
+    class Modes(Enum):
+        MENU = auto()
+        PLAYING = auto()
+        WINNER = auto()
+
     def __init__(self):
         self.game = Game(screen)
-        self.mode = "MENU"  # MENU / PLAYING / WINNER
+        self.mode = self.Modes.MENU
         self.BUTTONS = [
             Button("Beginner", (SCREEN_MIDDLE_X_POS, 300)),
             Button("Amateur", (SCREEN_MIDDLE_X_POS, 400)),
@@ -203,27 +209,26 @@ class GUI:
                     sys.exit()
 
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    if self.mode == "MENU":
+                    if self.mode == self.Modes.MENU:
                         for button in self.BUTTONS:
                             if button.is_hovered(self.mouse_pos):
-                                self.mode = "PLAYING"
+                                self.mode = self.Modes.PLAYING
                                 self.game.set_difficulty(button.text)
                                 self.draw_map(self.game.calculate_player_valid_moves())
                                 self.ui_loop()
 
-                    elif self.mode == "WINNER":
+                    elif self.mode == self.Modes.WINNER:
                         if self.reset_button.is_hovered(self.mouse_pos):
                             self.game = Game(screen)
-                            self.mode = "MENU"
+                            self.mode = self.Modes.MENU
 
-            if self.mode == "MENU":
+            if self.mode == self.Modes.MENU:
                 self.display_game_title()
 
-            elif self.mode == "PLAYING":
-                if self.game.winner:
-                    self.mode = "WINNER"
+            elif self.mode == self.Modes.PLAYING and self.game.winner:
+                self.mode = self.Modes.WINNER
 
-            elif self.mode == "WINNER":
+            elif self.mode == self.Modes.WINNER:
                 self.display_winner_screen()
             pygame.display.flip()
 
